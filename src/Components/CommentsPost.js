956040142cf300eb06970d58a6606c5607f22cb7
Anastasiia Volchanska <anastasiia_volchanska@epam.com>
2021-07-29 16:13:37 +0300

    added css for form

--- src/Components/CommentsPost.js
+++ src/Components/CommentsPost.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
 
 class PostCommentForm extends Component {
@@ -18,6 +18,7 @@ class PostCommentForm extends Component {
       .post('https://jordan.ashton.fashion/api/goods/30/comments', this.state)
       .then(response => {
         console.log(response)
+        alert("Comment added")
       })
       .catch(error => {
         console.log(error)
@@ -25,31 +26,34 @@ class PostCommentForm extends Component {
   }
 
   changeHandler = e => {
-    this.setState({ [e.target.name]: e.target.value })
+    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
-    const { name, text } = this.state
+    const {name, text} = this.state
     return (
       <div>
         <form onSubmit={this.submitHandler}>
           <div>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.changeHandler}
+            <input className={"form-input form-input-name"}
+                   type="text"
+                   name="name"
+                   value={name}
+                   onChange={this.changeHandler}
+                   placeholder="Your name:"
             />
           </div>
           <div>
-            <input
-              type="text"
-              name="text"
-              value={text}
-              onChange={this.changeHandler}
+            <input className={"form-input form-input-text"}
+
+                   type="text"
+                   name="text"
+                   value={text}
+                   onChange={this.changeHandler}
+                   placeholder="Type here your comment:"
             />
           </div>
-          <button type="submit">Submit</button>
+          <button type="submit" className={"form-bnt-submit"}>Submit</button>
         </form>
       </div>
     )
